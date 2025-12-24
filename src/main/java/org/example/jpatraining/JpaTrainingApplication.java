046ed34625c368504a87a451a27f9a744053c418
mohamedsoliman <salimanmohamed20@gmail.com>
2025-12-24 18:44:36 +0200

    First Commit

---
+++ src/main/java/org/example/jpatraining/JpaTrainingApplication.java
@@ -0,0 +1,13 @@
+package org.example.jpatraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jpatraining/JpaTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jpatraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
